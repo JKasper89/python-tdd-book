4d2ea932e1811adcf053468d815945aabaae8583
Jan Kasper <jan.kasper@students.tbs1.de>
2019-06-15 20:44:52 +0200

    Configured PyCharm as Editor

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,10 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-from django.template.loader import render_to_string
-
-from lists.views import home_page
 from lists.models import Item
+'from django.urls import resolve'
+'from django.http import HttpRequest'
+'from django.template.loader import render_to_string'
+'from lists.views import home_page'
+
 
 class HomePageTest(TestCase):
 
@@ -22,7 +22,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
@@ -36,6 +36,7 @@ class HomePageTest(TestCase):
 
         self.assertIn('itemey 1', response.content.decode())
         self.assertIn('itemey 2', response.content.decode())
+
 
 class ItemModelTest(TestCase):
 
@@ -55,3 +56,15 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewTest(TestCase):
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from lists.models import Item
 
+
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
+
+
+def view_list(request):
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
