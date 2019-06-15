60fe5a76967ae11bc398312ab4a13fc015a2c148
Jan Kasper <jan.kasper@students.tbs1.de>
2019-06-15 22:09:19 +0200

    URLs for forms, moved code in views+tests, new URL

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -72,7 +72,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied she goes back to sleep
 
-    def test_multiple_users_cas_start_lists_at_different_urls(self):
+    def test_multiple_users_can_start_lists_at_different_urls(self):
 
         # Edith starts a new to-do list
         self.browser.get(self.live_server_url)
